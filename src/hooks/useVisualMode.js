029cc34890a4c07be247b7b9bc2840bdde4e73d0
Jalayna Tipantiza <jalaynatipantiza@jalaynas-mbp.gateway.sasktel.net>
2020-07-09 19:30:15 -0600

    build custom hook  to share logic between components

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+export default function useVisualMode (initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]); 
+
+  const transition = (newMode, replace= false) => {
+    setMode(newMode)
+
+    setHistory(prev => replace ? [...prev.slice(0, prev.length -1), newMode]: [...prev, newMode])
+  }
+  const back = () => {
+    if(history.length > 1) {
+      history.pop();
+    }
+    setMode(history[history.length-1])
+  }
+  return {mode, transition, back};
+};
